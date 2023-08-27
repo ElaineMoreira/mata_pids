066fcebdfe8ab2653120a9778a68d41f4d2a1447
Elaine Moreira <elainefelipemoreira@gmail.com>
2023-08-27 10:36:47 -0300

    Adiciona README e arquivo pegaPID_mataPID

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mata_pids
+
+## automação em bash
+
+### para utilizar basta alterar o nome do processo que deseja matar na linha "pids=$(pgrep -f 'chrome')"
+
+### para executar basta digitar "./pegaPID_mataPID.sh"
---
+++ pegaPID_mataPID.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Pega os pids de todos os processos do chrome
+pids=$(pgrep -f 'chrome')
+    sleep 5
+echo "$pids"
+# Mata os processos existentes
+for pid in $pids; do
+    kill -9 $pid
+done
+    sleep 5
+echo "Processos encerrados com sucesso!"
